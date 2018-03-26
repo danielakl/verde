ca7f866ac09c1ce9402d1ec3ce42a61e443ecae7
Daniel Klock <danielkl@stud.ntnu.no>
2018-03-26 16:39:20 +0200

    Add webpacking for image- and css files Add repo information in package.json

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -14,6 +14,23 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.(s*)css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 64 * 1024, // Pack images < 64 KiB to base64 string.
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }]
             }
         ]
     }
